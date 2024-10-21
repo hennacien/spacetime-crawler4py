71ad98a5fa2a38184c5368c1549e3a1da0487307
hennacien <hknelson@uci.edu>
2024-10-21 11:34:59 -0700

    fixed ascii function

--- scraper.py
+++ scraper.py
@@ -17,34 +17,34 @@ def parse_link_content(link, resp):
     parsed_text = parsed_content.xpath('//body//text()') # gives a list of text
     text_string = ''.join(parsed_text).strip() # gives all the text formatted in a string
     totalWords, wordMappings = count_frequencies(tokenize(text_string))
-    largest_file(link, totalWords)
+    # largest_file(link, totalWords)
 
 # CONTINUE THIS 
-def largest_file(url, totalWords):
-    print(f"\nTOTAL WORDS {totalWords}\n")
-    print(f"\nURL HERE {url}\n")
-    if os.path.exists('largestFile.txt'):
-        with shelve.open('largestFile.txt', writeback=True) as shelf:
-            if 'url' in shelf and 'totalWords' in shelf:
-                if shelf['totalWords'] <= totalWords:
-                    shelf['url'] = url
-                    shelf['totalWords'] = totalWords
-            else:
-                shelf['url'] = url
-                shelf['totalWords'] = totalWords
-            shelf.sync()
+# def largest_file(url, totalWords):
+#     print(f"\nTOTAL WORDS {totalWords}\n")
+#     print(f"\nURL HERE {url}\n")
+#     if os.path.exists('largestFile.txt'):
+#         with shelve.open('largestFile.txt', writeback=True) as shelf:
+#             if 'url' in shelf and 'totalWords' in shelf:
+#                 if shelf['totalWords'] <= totalWords:
+#                     shelf['url'] = url
+#                     shelf['totalWords'] = totalWords
+#             else:
+#                 shelf['url'] = url
+#                 shelf['totalWords'] = totalWords
+#             shelf.sync()
 
                
-    else:
-        with open('largestFile.txt', 'w', encoding='utf-8') as file:
-            file.write(f'{url}\n')
-            file.write(f'{totalWords}\n')
+#     else:
+#         with open('largestFile.txt', 'w', encoding='utf-8') as file:
+#             file.write(f'{url}\n')
+#             file.write(f'{totalWords}\n')
 
 
 # NEED TO FIX THIS
 def is_ascii(character):
     ascii_code = ord(character)
-    return (ascii_code >= 48 and ascii_code <= 57) or (ascii_code >=65 and ascii_code >= 90) or (ascii_code >= 97 and ascii_code <= 122)
+    return (ascii_code >= 48 and ascii_code <= 57) or (ascii_code >=65 and ascii_code <= 90) or (ascii_code >= 97 and ascii_code <= 122)
 
 def tokenize(text_string):
     currWord = []
